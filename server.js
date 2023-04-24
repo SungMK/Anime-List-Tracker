7c2e824b909a16483fd7a061a81f7b5bd67ee7c6
Sung Kim <stwkim103@gmail.com>
2023-04-24 19:36:52 -0400

    Inital commit

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+const express = require('express');
+const logger = require('morgan');
+
+// Initialize Express App
+const app = express();
+
+// Environment Variables
+require ('dotenv').config();
+require ('./config/database');
+
+// Configure App Settings 
+app.set('view engine', 'ejs');
+
+// Middleware 
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+// Mounted Routes
+// app.use('/', indexRoutes);
+
+// Configure Port 3000
+app.listen(3000, () => {
+    console.log('Express is listening on Port:3000')
+});
