31e4d98225d99245c592380c1883a1b3574bef77
Sung Kim <stwkim103@gmail.com>
2023-04-27 23:16:11 -0400

    Implement delete functionality in show view

--- controllers/notes.js
+++ controllers/notes.js
@@ -14,9 +14,14 @@ async function create (req, res) {
 
 async function deleteNote (req, res) {
     try {
-        await Anime.notes.findByIdAndRemove(req.params.id);
-        // res.redirect(`/animes/${foundAnime._id}`);
-        res.redirect('/animes')
+        const anime = await Anime.findById(req.params.animeId);
+        anime.notes.forEach((n, i) => {
+            if(n._id == req.params.noteId) {
+                anime.notes.splice(i, 1)
+            }
+        })
+        await anime.save();
+        res.redirect(`/animes/${req.params.animeId}`);
     } catch (error) {
         console.log(error);
         res.render('error', {title: 'Something went wrong'}); 
@@ -25,5 +30,5 @@ async function deleteNote (req, res) {
 
 module.exports = {
     create,
-        delete: deleteNote,
+    delete: deleteNote,
 };
